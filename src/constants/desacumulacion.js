b008096a85076e998e4e8472238f067bcbd5b989
jaimepinzon <74882550+jaimepinzon@users.noreply.github.com>
2020-11-27 01:08:08 -0500

    Update desacumulacion.js

--- src/constants/desacumulacion.js
+++ src/constants/desacumulacion.js
@@ -8,47 +8,80 @@ export const desacumulacionGallery = {
   list: [
     {
       type: 'iframe',
-      thumb: 'video1.jpg',
-      value: 'https://www.youtube.com/embed/WO5tmfngerc'
+      thumb: 'desacum1.jpg',
+      value: 'https://www.youtube.com/embed/aKKJsySlsK4',
+      caption: 'Des-acumulación #1'
     },
     {
       value: 'desac1.jpg'
     },
     {
+      type: 'iframe',
+      thumb: 'desacum30.jpg',
+      value: 'https://www.youtube.com/embed/oIz0ao8PVsQ',
+      caption: 'Des-acumulación #30' 
+    },
+    {
       value: 'desac2.jpg'
+    },
+    {
+      type: 'iframe',
+      thumb: 'desacum37video.jpg',
+      value: 'https://www.youtube.com/embed/WO5tmfngerc'
     },
     {
       value: 'desac3.jpg'
     },
     {
       type: 'iframe',
-      thumb: 'video2.jpg',
-      value: 'https://www.youtube.com/embed/K5MLJLkUjXc'
+      thumb: 'desacum33.jpg',
+      value: 'https://www.youtube.com/embed/T6C1pYtomZI',
+      caption: 'Des-acumulación #33'
     },
     {
-      value: 'desac4.jpg'
+      value: 'desacumular2.jpg'
+    },
+    {
+      type: 'iframe',
+      thumb: 'desacum41video.jpg',
+      value: 'https://www.youtube.com/embed/K5MLJLkUjXc'
     },
     {
       value: 'desac5.jpg'
     },
     {
       type: 'iframe',
-      thumb: 'video3.jpg',
-      value: 'https://www.youtube.com/embed/dA-K3-1EMHg'
-    },
-    {
-      type: 'iframe',
-      thumb: 'video4.jpg',
-      value: 'https://www.youtube.com/embed/1yFy7yGw_bw'
+      thumb: 'desacum35.jpg',
+      value: 'https://www.youtube.com/embed/kxpxTBGys6g',
+      caption: 'Des-acumulación #35'
     },
     {
       value: 'desac6.jpg'
     },
     {
-      value: 'desac7.jpg'
+      type: 'iframe',
+      thumb: 'desacum42video.jpg',
+      value: 'https://www.youtube.com/embed/dA-K3-1EMHg'
     },
     {
-      value: 'desac8.jpg'
-    }
+      value: 'desacumular1.jpg'
+    },
+    {
+      type: 'iframe',
+      thumb: 'desacum43video.jpg',
+      value: 'https://www.youtube.com/embed/1yFy7yGw_bw'
+    },
+    {
+      value: 'desacumular3.jpg'
+    },
+    {
+      type: 'iframe',
+      thumb: 'desacum44.jpg',
+      value: 'https://www.youtube.com/embed/Sy3r0cLzCuU',
+      caption: 'Des-acumulación #44'
+    },
+    {
+      value: 'desac4.jpg'
+    }     
   ]
 }
